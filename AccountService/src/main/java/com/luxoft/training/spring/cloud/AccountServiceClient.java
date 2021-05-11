5d248adabfdabd0161077ba7e478f10796eec148
christina.gorina <christina.gorina@yandex.ru>
2021-05-11 18:59:30 +0300

    LabWork 2

---
+++ AccountService/src/main/java/com/luxoft/training/spring/cloud/AccountServiceClient.java
@@ -0,0 +1,13 @@
+package com.luxoft.training.spring.cloud;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import java.math.BigDecimal;
+@FeignClient(name = "AccountService", fallback = AccountServiceFallback.class)
+public interface AccountServiceClient {
+    @RequestMapping("/checkout/{id}")
+    boolean checkout(@PathVariable("id") Integer accountId,
+                     @RequestParam("sum") BigDecimal sum);
+}
